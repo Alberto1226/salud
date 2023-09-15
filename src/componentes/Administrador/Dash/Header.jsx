af319fd530c5e951055aa5591f06414459a62289
DavidFranco3 <josedavidayalafranco3@gmail.com>
2023-09-15 13:02:21 -0600

    se agrega toda la logica del login, validacion de usuarios

--- src/componentes/Administrador/Dash/Header.jsx
+++ src/componentes/Administrador/Dash/Header.jsx
@@ -1,6 +1,35 @@
+import { NavBar } from "../../Home/NavBar/NavBar";
+import { listarSeries } from "../../../api/series";
+import { useState, useEffect } from "react";
+
 export function HeaderDash() {
+  const [listarSer, setListSeries] = useState([]);
+
+  const obtenerSeries = () => {
+    try {
+      listarSeries()
+        .then((response) => {
+          const { data } = response;
+
+          if (!listarSer && data) {
+            setListSeries(formatModelSeries(data));
+            console.log(data);
+          } else {
+            const datosSer = formatModelSeries(data);
+            setListSeries(datosSer);
+          }
+        })
+        .catch((e) => { });
+    } catch (e) { }
+  };
+
+  useEffect(() => {
+    obtenerSeries();
+  }, []);
+
   return (
     <>
+    <NavBar listarSeries={listarSer}/>
       <nav className="main-header navbar navbar-expand navbar-white navbar-light">
         {/* Left navbar links */}
         <ul className="navbar-nav">
@@ -26,4 +55,31 @@ export function HeaderDash() {
       </nav>
     </>
   );
+}
+
+function formatModelSeries(data) {
+  const dataTemp = [];
+  data.forEach((data) => {
+    dataTemp.push({
+      id: data._id,
+      titulo: data.titulo,
+      categorias: data.categorias,
+      actores: data.actores,
+      director: data.director,
+      duracion: data.duracion,
+      tipo: "series",
+      sinopsis: data.sinopsis,
+      calificacion: data.calificacion,
+      datosTemporada: data.datosTemporada,
+      año: data.año,
+      disponibilidad: data.disponibilidad,
+      masVisto: data.masVisto,
+      recomendado: data.recomendado,
+      urlVideo: data.urlTrailer,
+      urlPortada: data.urlPortada,
+      seccion: data.seccion,
+      estado: data.estado,
+    });
+  });
+  return dataTemp;
 }
--- src/pages/Dashboard/dashboard.jsx
+++ src/pages/Dashboard/dashboard.jsx
@@ -11,8 +11,6 @@ import {
 } from "../../api/auth";
 import { Error } from '../Error404/error';
 import { useState, useEffect } from "react";
-import { listarSeries } from '../../api/series';
-import { NavBar } from '../../componentes/Home/NavBar/NavBar';
 
 export function Dashboard() {
   const [tipoUsuario, setTipoUsuario] = useState();
@@ -37,37 +35,12 @@ export function Dashboard() {
     obtenerDatosUsuario();
   }, []);
 
-  const [listarSer, setListSeries] = useState([]);
-
-  const obtenerSeries = () => {
-    try {
-      listarSeries()
-        .then((response) => {
-          const { data } = response;
-
-          if (!listarSer && data) {
-            setListSeries(formatModelSeries(data));
-            console.log(data);
-          } else {
-            const datosSer = formatModelSeries(data);
-            setListSeries(datosSer);
-          }
-        })
-        .catch((e) => { });
-    } catch (e) { }
-  };
-
-  useEffect(() => {
-    obtenerSeries();
-  }, []);
-
   return (
     <>
       {
         (tipoUsuario == "administrador" || tipoUsuario == "Medico") ?
           (
             <>
-              {/*<NavBar listarSeries={listarSer}/>*/}
               <HeaderDash />
               <MenuDash />
               <BodyDash />
@@ -82,31 +55,4 @@ export function Dashboard() {
       }
     </>
   );
-}
-
-function formatModelSeries(data) {
-  const dataTemp = [];
-  data.forEach((data) => {
-    dataTemp.push({
-      id: data._id,
-      titulo: data.titulo,
-      categorias: data.categorias,
-      actores: data.actores,
-      director: data.director,
-      duracion: data.duracion,
-      tipo: "series",
-      sinopsis: data.sinopsis,
-      calificacion: data.calificacion,
-      datosTemporada: data.datosTemporada,
-      año: data.año,
-      disponibilidad: data.disponibilidad,
-      masVisto: data.masVisto,
-      recomendado: data.recomendado,
-      urlVideo: data.urlTrailer,
-      urlPortada: data.urlPortada,
-      seccion: data.seccion,
-      estado: data.estado,
-    });
-  });
-  return dataTemp;
 }
